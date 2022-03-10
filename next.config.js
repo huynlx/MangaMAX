daa986bc31f021c8fd1c591e8bc33da3d33acf46
Lê Xuân Huynh <huynhlx62@wru.vn>
2022-03-10 12:17:17 +0700

    chore: remove next img

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: ['st.nettruyengo.com'],
-  }
+  reactStrictMode: true
 }
 
 module.exports = nextConfig
