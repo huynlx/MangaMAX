a6aa49b5d60aacf7b4ac6b49ba0175f647662c76
Lê Xuân Huynh <huynhlx62@wru.vn>
2023-10-13 19:33:10 +0700

    hot fix

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,116 +1,122 @@
 import { mangaProps } from "@/components/Info/LeftComic";
 import { RootState } from "@/store";
 
-export const regexMatchMultiString = /NhatTruyen|TruyenQQ|NetTruyen|Cmangavip.com|Cmanga/g;
+export const regexMatchMultiString =
+  /NhatTruyen|TruyenQQ|NetTruyen|Cmangavip.com|Cmanga/g;
 
 export const API_URL = "https://www.nettruyenup.com/";
 
 export const SOURCES = [
-    {
-        source: '1',
-        name: 'Server 1',
-        url: 'https://www.nettruyenup.com/',
-        type: 'latest',
-        server: "nettruyen",
-        adult: false
-    },
-    {
-        source: '2',
-        name: 'Server 2',
-        url: 'https://nhattruyenmin.com/',
-        type: 'latest',
-        server: 'netruyen',
-        adult: false
-    },
-    {
-        source: '3',
-        name: 'Server 3 (18+)',
-        url: 'https://lxmanga.net/',
-        type: 'latest',
-        server: 'lxhentai',
-        adult: true
-    },
-    {
-        source: '4',
-        name: 'Server 4',
-        url: 'https://truyenqqq.vn/',
-        type: 'latest',
-        server: 'truyen48',
-        adult: false
-    },
-    {
-        source: '5',
-        name: 'Server 5',
-        url: 'https://truyentranhlh.net/',
-        type: 'latest',
-        server: 'truyentranhlh',
-        adult: false
-    },
-    {
-        source: '6',
-        name: 'Server 6',
-        url: 'https://blogtruyen.vn/',
-        type: 'latest',
-        server: 'blogtruyen',
-        adult: false
-    },
-    {
-        source: '7',
-        name: 'Server 7',
-        url: 'https://cmangaac.com/',
-        type: 'latest',
-        server: 'cmanga',
-        adult: false
-    },
-    {
-        source: '8',
-        name: 'Server 8',
-        url: 'https://comick.xyz/',
-        type: 'latest',
-        server: 'comick',
-        adult: false
-    },
-    {
-        source: '9',
-        name: 'Server 9 (18+)',
-        url: 'https://cbhentai.xyz/',
-        type: 'latest',
-        server: 'hentaicube',
-        adult: true
-    },
-    {
-        source: '10',
-        name: 'Server 10',
-        url: 'https://mangaii.com/',
-        type: 'latest',
-        server: 'mangaii',
-        adult: false
-    }
+  {
+    source: "1",
+    name: "Server 1",
+    url: "https://www.nettruyenus.com/",
+    type: "latest",
+    server: "nettruyen",
+    adult: false,
+  },
+  {
+    source: "2",
+    name: "Server 2",
+    url: "https://nhattruyenplus.com/",
+    type: "latest",
+    server: "netruyen",
+    adult: false,
+  },
+  {
+    source: "3",
+    name: "Server 3 (18+)",
+    url: "https://lxmanga.net/",
+    type: "latest",
+    server: "lxhentai",
+    adult: true,
+  },
+  {
+    source: "4",
+    name: "Server 4",
+    url: "https://truyenqqvn.com/",
+    type: "latest",
+    server: "truyen48",
+    adult: false,
+  },
+  {
+    source: "5",
+    name: "Server 5",
+    url: "https://truyentranhlh.net/",
+    type: "latest",
+    server: "truyentranhlh",
+    adult: false,
+  },
+  {
+    source: "6",
+    name: "Server 6",
+    url: "https://blogtruyen.vn/",
+    type: "latest",
+    server: "blogtruyen",
+    adult: false,
+  },
+  {
+    source: "7",
+    name: "Server 7",
+    url: "https://cmangaaz.com/",
+    type: "latest",
+    server: "cmanga",
+    adult: false,
+  },
+  {
+    source: "8",
+    name: "Server 8",
+    url: "https://comick.xyz/",
+    type: "latest",
+    server: "comick",
+    adult: false,
+  },
+  {
+    source: "9",
+    name: "Server 9 (18+)",
+    url: "https://hentaicube.net/",
+    type: "latest",
+    server: "hentaicube",
+    adult: true,
+  },
+  {
+    source: "10",
+    name: "Server 10",
+    url: "https://mangaii.com/",
+    type: "latest",
+    server: "mangaii",
+    adult: false,
+  },
 ];
 
 export const WINDOW_SIZE = {
-    mobile: 320,
-    phablet: 480,
-    tablet: 768,
-    laptop: 992,
-    desktop: 1200,
-    all: 0
+  mobile: 320,
+  phablet: 480,
+  tablet: 768,
+  laptop: 992,
+  desktop: 1200,
+  all: 0,
 };
 
 export const WINDOW_RESIZE_DEBOUNCE = 400;
 
-export const mangaObj = (info: any, slug: string, select: RootState, type: string): mangaProps => ({
-    title: info.title,
-    cover: info.cover,
-    slug: slug,
-    url: `/manga/${slug}?source=${info.source ?? select?.source}&type=${type}`,
-    source: info.source ?? select?.source,
-    type: type
+export const mangaObj = (
+  info: any,
+  slug: string,
+  select: RootState,
+  type: string
+): mangaProps => ({
+  title: info.title,
+  cover: info.cover,
+  slug: slug,
+  url: `/manga/${slug}?source=${info.source ?? select?.source}&type=${type}`,
+  source: info.source ?? select?.source,
+  type: type,
 });
 
-export const TITLES = ['latest', 'browse'];
-export const FORYOU = ['recents', 'bookmarks'];
+export const TITLES = ["latest", "browse"];
+export const FORYOU = ["recents", "bookmarks"];
 
-export const WEBSITE_URL = 'https://mangamax-huynh.cf/';
+export const WEBSITE_URL = "https://mangamax-huynh.cf/";
 
 export const GA_MEASUREMENT_ID = "G-MGL73630CW";
--- src/shared/truyentranhlh/chapter.ts
+++ src/shared/truyentranhlh/chapter.ts
@@ -2,22 +2,29 @@ import { parse } from "node-html-parser";
 import decodeHTMLEntity from "@/utils/decodeHTML";
 import axios from "@/utils/axios";
 
-export const getChapter = async (source: string, comicSLug: any, chapterSLug: any, chapterId: any): Promise<any> => {
-  const links = [
-    `truyen-tranh/${comicSLug}/${chapterSLug}`
-  ];
+export const getChapter = async (
+  source: string,
+  comicSLug: any,
+  chapterSLug: any,
+  chapterId: any
+): Promise<any> => {
+  const links = [`truyen-tranh/${comicSLug}/${chapterSLug}`];
 
-  const html = await Promise.all(links.map(async (link) => (await axios.get(link)).data));
+  const html = await Promise.all(
+    links.map(async (link) => (await axios.get(link)).data)
+  );
   const dom = parse(html[0]);
 
   return {
-    title: decodeHTMLEntity(dom.querySelector(".rd_sidebar-name h5 a")?.innerText),
+    title: decodeHTMLEntity(
+      dom.querySelector(".rd_sidebar-name h5 a")?.innerText
+    ),
     chapterCurrent: dom.querySelector("#chap_list .current a")?.innerText,
-    updateAt: '?',
-    images: dom.querySelectorAll("#chapter-content img").map(img => {
-      let url = img.getAttribute("data-src") ?? img.getAttribute("src");
-      return url;
+    updateAt: "?",
+    images: dom.querySelectorAll("#chapter-content img").map((img) => {
+      let url = img.getAttribute("data-src") ?? img.getAttribute("src") ?? "";
+      return `/api/proxy2?url=${encodeURIComponent(url)}&source=${source}`;
     }),
-    source
-  }
-}
+    source,
+  };
+};
