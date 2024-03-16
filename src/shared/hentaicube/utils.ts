3ad65f605a7de548100d301e06af8f3066bb74dc
Lê Xuân Huynh <huynhlx62@wru.vn>
2024-03-16 12:38:36 +0700

    fix cover hentai cube

--- src/shared/hentaicube/utils.ts
+++ src/shared/hentaicube/utils.ts
@@ -1,8 +1,6 @@
-export const getCover = (image?: string) =>
-  `https://apoqrsgtqq.cloudimg.io/${image}?width=193`;
+export const getCover = (image?: string) => `${image}?width=193`;
 
-export const getCoverOrigin = (image?: string) =>
-  `https://apoqrsgtqq.cloudimg.io/${image}`;
+export const getCoverOrigin = (image?: string) => `${image}`;
 
 export const black_list: string[] = [
   "https://cdn1.hentaicube.net/ext/2022/9/1/nha-tu-vat-sua/3/00.jpg",
@@ -15,5 +13,5 @@ export const black_list: string[] = [
   "https://cdn1.hentaicube.net/ext/2019/09/03/tittylating/003.jpg",
   "https://cdn1.hentaicube.net/ext/2019/12/30/mama-sensei-and-me/01.jpg",
   "https://cdn1.hentaicube.net/ext/2020/03/01/my-gloomy-self-used-these-magic-items/1/01.jpg",
-  "https://cdn1.hentaicube.net/ext/2020/09/1/enji-ni-somaru/1/03.jpg"
+  "https://cdn1.hentaicube.net/ext/2020/09/1/enji-ni-somaru/1/03.jpg",
 ];
